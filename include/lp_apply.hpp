53dd5d3af7d880bf34fb64bcf08459023077d6b1
ExuberantIris <imanthony1010.gmail.com>
2026-06-14 18:47:04 +0800

    New update

--- include/lp_apply.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstddef>
-
-#include "lp_types.hpp"
-
-int lp_apply_solution(PdDesign *d, const LpProblem *pb, const LpSolution *sol, char *err,
-                      std::size_t err_sz);
--- include/pd_clock.h
+++
@@ -1,8 +0,0 @@
-#ifndef PD_CLOCK_H
-#define PD_CLOCK_H
-
-#include "pd_types.h"
-
-void pd_annotate_clock(PdDesign *d);
-
-#endif
